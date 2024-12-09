ff07f3cee07f35d04d676d78f5d6891a324e5431
Garrett Trojanek <trojanekga@gmail.com>
2024-12-08 21:06:32 -0600

    Create basic add/subtract functionality for number 1

--- scripts.js
+++ scripts.js
@@ -51,6 +51,10 @@ let clear = document.querySelector(".clear");
 let minus = document.querySelector(".minus");
 let equals = document.querySelector(".equals");
 let output = document.querySelector(".output");
+let firstNumber;
+let secondNumber;
+let total;
+let operation;
 
 clear.addEventListener("click", function() {
     clearResult();
@@ -66,20 +70,57 @@ zero.addEventListener("click", function() {
     }
 });
 one.addEventListener("click", function() {
-    if (firstNum = true){
-        console.log("One");
-        output.value = (output.value + "1");
+    if (firstNum == true){
+        if (firstNumber == null){
+            firstNumber = 1;
+            console.log(firstNumber);
+            output.value = (output.value + "1");
+        }
+        else{
+                
+                firstNumber = firstNumber + "1";
+                console.log(firstNumber);
+                output.value = (output.value + "1");
+            }
     }
     else {
-        output.value = (output.value + "1");
+        if (secondNumber == null){
+            secondNumber = 1;
+            console.log(secondNumber);
+            output.value = (output.value + "1");
+        }
+        else{
+            secondNumber = secondNumber + "1";
+            console.log(secondNumber);
+            output.value = (output.value + "1");
+        }
     }
 
 });
 
 add.addEventListener("click", function() {
     firstNum = false;
-    a = a;
     output.value = (output.value + "+");
+    operation = "+";
+})
+
+minus.addEventListener("click", function() {
+    firstNum = false;
+    output.value = (output.value + "-");
+    operation = "-";
+})
+
+equals.addEventListener("click", function(){
+    if (operation == "+"){
+        total = parseInt(firstNumber) + parseInt(secondNumber);
+        output.value = (output.value + "=" + total);
+    }
+
+    else if (operation == "-"){
+        total = parseInt(firstNumber) - parseInt(secondNumber);
+        output.value = (output.value + "=" + total);
+    }
+
 })
 
 function clearResult (){
